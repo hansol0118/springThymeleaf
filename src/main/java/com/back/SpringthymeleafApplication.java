22db5b772d0f9b4e7cffd51df8e314e2f1cccea3
hansol0118 <wall197944@naver.com>
2026-03-05 11:32:20 +0900

    2-초기 설정 및 샘플 데이터 생성

--- src/main/java/com/back/SpringthymeleafApplication.java
+++ src/main/java/com/back/SpringthymeleafApplication.java
@@ -1,9 +1,11 @@
-package com.back.springthymeleaf;
+package com.back;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.data.jpa.repository.config.EnableJpaAuditing;
 
 @SpringBootApplication
+@EnableJpaAuditing
 public class SpringthymeleafApplication {
 
     public static void main(String[] args) {
---
+++ src/main/java/com/back/global/initData/BaseInitData.java
@@ -0,0 +1,32 @@
+package com.back.global.initData;
+
+import com.back.domain.post.service.PostService;
+import lombok.RequiredArgsConstructor;
+import org.springframework.boot.ApplicationRunner;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+
+@Configuration
+@RequiredArgsConstructor
+public class BaseInitData {
+
+    private final PostService postService;
+
+    @Bean
+    public ApplicationRunner initData(){
+        return args -> {
+            work1();
+        };
+    }
+    public void work1(){
+        System.out.println("초기 데이터 작업");
+
+        if(postService.count()>0){
+            return;
+        }
+        postService.write("제목1","내용1");
+        postService.write("제목2","내용2");
+        postService.write("제목3","내용3");
+
+    }
+}
