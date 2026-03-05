e619cce06d62a82a84afe606aefbeeacc98060ea
hansol0118 <wall197944@naver.com>
2026-03-05 14:30:39 +0900

    8

--- src/main/java/com/back/domain/post/controller/PostController.java
+++ src/main/java/com/back/domain/post/controller/PostController.java
@@ -18,7 +18,7 @@ public class PostController {
     @ResponseBody
     public String writeForm() {
 
-        return getWriteForm();
+        return getWriteForm("", "", "", "");
     }
 
     @PostMapping("/posts/write")
@@ -26,36 +26,40 @@ public class PostController {
     public String write(String title, String content) {
 
         // 유효성 체크
-        if(title.isBlank()) {
-            return """
-                    <div style="color:red">제목을 입력해주세요.</div>
-                    %s
-                    """.formatted(getWriteForm());
+        if (title.isBlank()) {
+            return getWriteForm("제목을 입력해주세요.", title, content, "title");
         }
 
-        if(content.isBlank()) {
-            return """
-                    <div style="color:red">내용을 입력해주세요.</div>
-                    %s
-                    """.formatted(getWriteForm());
+        if (content.isBlank()) {
+            return getWriteForm("내용을 입력해주세요.", title, content, "content");
         }
-        //
+
 
         Post post = postService.write(title, content);
 
         return "%d번 글이 작성되었습니다.".formatted(post.getId());
     }
 
-    private String getWriteForm() {
+    private String getWriteForm(String errorMessage, String title, String content, String errorFieldName) {
         return """
+                <div style="color:red">%s</div>
                 <form method="post" action="/posts/write">
-                  <input type="text" name="title" autoFocus>
+                  <input type="text" name="title" value="%s" autoFocus>
                   <br>
-                  <textarea name="content"></textarea>
+                  <textarea name="content">%s</textarea>
                   <br>
                   <input type="submit" value="작성">
                 </form>
-                """;
+                
+                <script>
+                    const errorFieldName = "%s";
+                
+                    if(errorFieldName.length > 0) {
+                        const form = document.querySelector("form");
+                        form[errorFieldName].focus();
+                    }
+                </script>
+                """.formatted(errorMessage, title, content, errorFieldName);
     }
 
 }
