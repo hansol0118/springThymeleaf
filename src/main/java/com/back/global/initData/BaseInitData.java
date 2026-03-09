dff3d47bee5f7e0671f4cd1dcbfb625386bc606c
hansol0118 <wall197944@naver.com>
2026-03-09 12:32:49 +0900

    32-39

--- src/main/java/com/back/global/initData/BaseInitData.java
+++ src/main/java/com/back/global/initData/BaseInitData.java
@@ -1,6 +1,7 @@
 package com.back.global.initData;
 
-import com.back.domain.post.service.PostService;
+import com.back.domain.post.post.entity.Post;
+import com.back.domain.post.post.service.PostService;
 import lombok.RequiredArgsConstructor;
 import org.springframework.boot.ApplicationRunner;
 import org.springframework.context.annotation.Bean;
@@ -11,11 +12,12 @@ import org.springframework.context.annotation.Configuration;
 public class BaseInitData {
 
     private final PostService postService;
+    private BaseInitData self;
 
     @Bean
     public ApplicationRunner initData(){
         return args -> {
-            work1();
+            self.work1();
         };
     }
     public void work1(){
@@ -27,6 +29,16 @@ public class BaseInitData {
         postService.write("제목1","내용1");
         postService.write("제목2","내용2");
         postService.write("제목3","내용3");
+
+        Post post1 = postService.write("제목1", "내용1");
+        Post post2 = postService.write("제목2", "내용2");
+        Post post3 = postService.write("제목3", "내용3");
+
+        post1.addComment("댓글 1-1");
+        post1.addComment("댓글 1-2");
+        post1.addComment("댓글 1-3");
+        post2.addComment("댓글 2-1");
+        post2.addComment("댓글 2-2");
 
     }
 }
